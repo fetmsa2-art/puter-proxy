b75813a9854d9dff33a07c411b075466e71bfe02
fetmsa2-art <fetmsa2@gmail.com>
2026-06-01 16:19:53 +0500

    server.js

--- server.js
+++ server.js
@@ -45,4 +45,5 @@ app.post('/v1/chat/completions', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Puter Proxy running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Puter Proxy running on port ${PORT}`));
